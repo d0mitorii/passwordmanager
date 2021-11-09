dca6f33814fce25c5e4d275c9bd2f4a662b6dcce
d0mitorii <domitoriiforwork@gmail.com>
2021-11-09 11:44:34 +0300

    transferring variables for the db to the config.json

--- app/database.py
+++ app/database.py
@@ -1,6 +1,4 @@
 from flask_sqlalchemy import SQLAlchemy
 from app import app
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
